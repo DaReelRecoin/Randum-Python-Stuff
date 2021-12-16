b782480dfdbf9da6ae52da53ce596e41ed3d52cc
Recoin <96213865+DaReelRecoin@users.noreply.github.com>
2021-12-15 16:57:43 -0800

    the all you can edit madlib

---
+++ madlib.py
@@ -0,0 +1,12 @@
+
+print("---Create Your MadLib---")
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+item_of_choice = input("Enter the name of something or someone: ")
+
+print("---Generated MadLib---")
+    
+print("My favorite color is " + color)
+print(plural_noun + " are really cool")
+print("But the one thing I hate is " + item_of_choice)
